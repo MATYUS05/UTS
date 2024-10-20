365f103d28ba6957d23de221c8fc448fc96d0585
SebastianObert <163373214+SebastianObert@users.noreply.github.com>
2024-10-20 15:27:15 +0700

    Update db.php

--- Project-lab/db.php
+++ Project-lab/db.php
@@ -4,18 +4,10 @@ $username = "root";
 $password = "";
 $dbname = "project_lab";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Protect from SQL Injection
-function sanitize_input($data) {
-    global $conn;
-    $data = htmlspecialchars($data); // Prevent XSS
-    return $conn->real_escape_string($data);
-}
-?>
+
